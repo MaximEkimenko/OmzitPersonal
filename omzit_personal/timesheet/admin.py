8c012d97a1590268c35dfe73f4888716cb25224f
Чекаловец А.В. <user-357@omep.net.ru>
2023-10-31 15:43:31 +0600

    created models Emloyee, Timesheet, DayStatus, registred in admin

---
+++ omzit_personal/timesheet/admin.py
@@ -0,0 +1,123 @@
+from django.contrib import admin
+
+from timesheet.models import Employee, Timesheet, DayStatus
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    all_fields = [
+        "fio",
+        "employment_date",
+        "fired_date",
+        "job_title",
+        "rank_title",
+        "tariff_rate",
+        "id_1C",
+        "division",
+        "status",
+        "schedule",
+        "shift_hours",
+        "skud_access",
+        "day_start",
+        "boss",
+        "KTR_category",
+        "KTR",
+        "has_NAX",
+        "KNAX",
+    ]
+    list_display = all_fields
+    list_display_links = ["fio"]
+    search_fields = ["fio"]
+    list_filter = all_fields
+    list_editable = [
+        "status",
+        "schedule",
+        "shift_hours",
+        "skud_access",
+        "day_start",
+        "boss",
+        "KTR_category",
+        "KTR",
+        "has_NAX",
+        "KNAX"
+    ]
+
+
+@admin.register(Timesheet)
+class TimesheetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "fio",
+                    "day_status",
+                    "date",
+                ],
+            },
+        ),
+        (
+            "Дневная смена",
+            {
+                "fields": [
+                    "skud_day_start",
+                    "skud_day_end",
+                    "skud_day_duration",
+                    "boss_day_duration",
+                    "is_day_corrected",
+                ],
+                "classes": ["collapse"],
+            },
+        ),
+        (
+            "Ночная смена",
+            {
+                "fields": [
+                    "skud_night_start",
+                    "skud_night_end",
+                    "skud_night_duration",
+                    "boss_night_duration",
+                    "is_night_corrected",
+                ],
+                "classes": ["collapse"],
+            },
+        ),
+    ]
+    list_display = [
+        "fio",
+        "day_status",
+        "date",
+        "skud_day_start",
+        "skud_day_end",
+        "skud_day_duration",
+        "boss_day_duration",
+        "is_day_corrected",
+        "skud_night_start",
+        "skud_night_end",
+        "skud_night_duration",
+        "boss_night_duration",
+        "is_night_corrected",
+    ]
+    list_display_links = ["fio"]
+    search_fields = ["fio"]
+    list_filter = [
+        "day_status",
+        "date",
+        "skud_day_duration",
+        "boss_day_duration",
+        "skud_night_duration",
+        "boss_night_duration",
+        "is_day_corrected",
+        "is_night_corrected",
+    ]
+    list_editable = [
+        "boss_day_duration",
+        "boss_night_duration",
+    ]
+
+
+@admin.register(DayStatus)
+class DayStatusAdmin(admin.ModelAdmin):
+    list_display = ["symbol", "name"]
+    list_display_links = ["symbol"]
+    search_fields = ["symbol", "name"]
